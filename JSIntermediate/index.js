5b374de7edff416c0b284d9e98d9ad335857aee5
Nicole Kelly <nrkelly57@gmail.com>
2023-08-13 16:46:24 +1200

    commit all

--- JSIntermediate/index.js
+++ JSIntermediate/index.js
@@ -1,3 +1,7 @@
-const string = "Everyone's got a plan until they get punched in the mouth";
+// const string = "Everyone's got a plan until they get punched in the mouth";
 
-console.log(string.ucFirstLetters("punched in the mouth"));
+// console.log(string.ucFirstLetters("punched in the mouth"));
+
+const string = 'I could be anything you like'
+
+console.log(string.toUpperCase()) // I COULD BE ANYTHING YOU LIKE
